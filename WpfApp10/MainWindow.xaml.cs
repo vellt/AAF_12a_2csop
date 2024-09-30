e18a7072a3aad52368ac12a14e91f966f24f13b8
vellt <szantobenjamin97@gmail.com>
2024-09-30 18:34:46 +0200

    mai ora

---
+++ WpfApp10/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp10
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            gomb.Click += Gomb_Click;
+            
+        }
+
+        private void Gomb_Click(object sender, RoutedEventArgs e)
+        {
+            if (ker.Text.Trim().Length==0)
+            {
+                MessageBox.Show("*-al jelölt mezőket kötelező kitölteni!");
+            }
+            else
+            {
+                MessageBox.Show($"A nevem {ker.Text}\n{vez.Text} {ker.Text}", "007");
+            }
+        }
+    }
+}
