d524029b74800f976e59af7cae5b00b5b66b4c9b
vellt <szantobenjamin97@gmail.com>
2025-02-13 18:16:10 +0100

    mai ora

--- WpfApp83/MainWindow.xaml.cs
+++ WpfApp83/MainWindow.xaml.cs
@@ -12,17 +12,85 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Media;
 
 namespace WpfApp83
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    class Dino
+    {
+        public string Nev { get; set; }
+        public string Kep { get; set; }
+        public string Leiras { get; set; }
+    }
     public partial class MainWindow : Window
     {
+        SoundPlayer player = new SoundPlayer();
+        bool isPlaying = true;
         public MainWindow()
         {
             InitializeComponent();
+            nevek.ItemsSource = ReadData();
+            nevek.DisplayMemberPath = "Nev";
+            nevek.SelectionChanged += Nevek_SelectionChanged;
+            nevek.SelectedIndex = 0;
+            elozo.Click += Elozo_Click;
+            kovetkezo.Click += Kovetkezo_Click;
+            player.SoundLocation = "africa.wav";
+            player.PlayLooping();
+            zene.MouseUp += Zene_MouseUp;
+        }
+
+        private void Zene_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Image ikon = zene.Children[0] as Image;
+            if (isPlaying)
+            {
+                player.Stop();
+                isPlaying = false;
+                ikon.Source = new BitmapImage(new Uri("kepek/mute.png", UriKind.Relative));
+            }
+            else // ez az az eset amikor az isplaying false értékű
+            {
+                player.PlayLooping();
+                isPlaying = true;
+                ikon.Source = new BitmapImage(new Uri("kepek/unmute.png", UriKind.Relative));
+            }
+        }
+
+        private void Kovetkezo_Click(object sender, RoutedEventArgs e)
+        {
+            nevek.SelectedIndex += 1;
+        }
+
+        private void Elozo_Click(object sender, RoutedEventArgs e)
+        {
+            if (nevek.SelectedIndex!=0)
+            {
+                nevek.SelectedIndex -= 1;
+            }
+            
+        }
+
+        private void Nevek_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Dino kivalasztottDino = nevek.SelectedItem as Dino;
+            szoveg.Text = kivalasztottDino.Leiras;
+            kep.Source = new BitmapImage(new Uri($"kepek/{kivalasztottDino.Kep}", UriKind.Relative));
+            sorszam.Text = $"{nevek.SelectedIndex+1}/7";
+        }
+
+        List<Dino> ReadData()
+        {
+            return File.ReadAllLines("dinok.csv")
+                .Skip(1)
+                .Select(x=>x.Split(';'))
+                .Select(x=>new Dino()
+                {
+                    Nev=x[0],
+                    Kep=x[1],
+                    Leiras=x[2]
+                }).ToList();
         }
     }
 }
