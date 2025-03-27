53bda4898eae70442d087ceeeb2afc54a01ead0f
vellt <szantobenjamin97@gmail.com>
2025-03-27 19:35:16 +0100

    mai ora

---
+++ WpfApp108/Models/Cica.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp108.Models
+{
+    //id;name;age;gender;fur_length;image_path
+    class Cica
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string FurLenght { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
