f10c26545a7ce1716c90cb89db2bc04bcfbfc6c1
vellt <szantobenjamin97@gmail.com>
2025-04-03 23:05:10 +0200

    mai ora

---
+++ WpfApp114/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json; // annotációk miatt
+
+namespace WpfApp114.Models
+{
+    class Movie
+    {
+        [JsonProperty("age_rating")]
+        public int AgeRating { get; set; } // korhatár
+        [JsonProperty("genre")]
+        public string Genre { get; set; } // műfaj
+        [JsonProperty("image")]
+        public string Image { get; set; } // poszter
+        [JsonProperty("title")]
+        public string Title { get; set; }  // cím
+    }
+}
