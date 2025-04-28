9e3835d52cd5c71b1411b7e02714cd903b6d1cae
vellt <szantobenjamin97@gmail.com>
2025-04-28 13:57:30 +0200

    mai ora

---
+++ WpfApp121/Models/Shoe.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace WpfApp121.Models
+{
+    class Shoe
+    {
+        [JsonProperty("category")]
+        public string Category { get; set; }
+        [JsonProperty("image")]
+        public string Image { get; set; }
+        [JsonProperty("link")]
+        public string Link { get; set; }
+        [JsonProperty("price")]
+        public string Price { get; set; }
+        [JsonProperty("title")]
+        public string Title { get; set; }
+    }
+}
